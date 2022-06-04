57f64693604bfd92f9aa3ff14285b31f1644fe3a
Pablo Romeo <pablo@cloudx.com>
2022-06-04 02:13:05 -0300

    Initial implementation of Transcoding Relay

--- README.md
+++ README.md
@@ -96,6 +96,7 @@ services:
       TZ: Europe/London
       ORCHESTRATOR_URL: http://plex-orchestrator:3500
       PMS_IP: 192.168.2.1
+      PMS_PORT: 32400
       TRANSCODE_OPERATING_MODE: both #(local|remote|both)
       TRANSCODER_VERBOSE: "1"   # 1=verbose, 0=silent
     healthcheck:
@@ -284,6 +285,7 @@ The image extends the [LinuxServer Plex](https://hub.docker.com/r/linuxserver/pl
 | :----: | --- |
 | `ORCHESTRATOR_URL` | The url where the orchestrator service can be reached (ex: http://plex-orchestrator:3500) |
 | `PMS_IP` | IP pointing at the Plex instance (can be the cluster IP) |
+| `PMS_PORT` | Port used by Plex (default 32400) |
 | `TRANSCODE_EAE_LOCALLY` | Force media which requires EasyAudioEncoder to transcode locally |
 | `TRANSCODE_OPERATING_MODE` | "local" => only local transcoding (no workers), "remote" => only remote workers transcoding, "both" (default) => Remote first, local if it fails |
 | `TRANSCODER_VERBOSE` | "0" (default) => info level, "1" => debug logging |
--- pms/app/transcoder.js
+++ pms/app/transcoder.js
@@ -3,6 +3,7 @@ const ORCHESTRATOR_URL = process.env.ORCHESTRATOR_URL || 'http://localhost:3500'
 const TRANSCODER_PATH = process.env.TRANSCODER_PATH || '/usr/lib/plexmediaserver/'
 const TRANSCODER_LOCAL_NAME = process.env.TRANSCODER_LOCAL_NAME || 'originalTranscoder'
 const PMS_IP = process.env.PMS_IP || '127.0.0.1'
+const PMS_PORT = process.env.PMS_PORT || '32400'
 const TRANSCODER_VERBOSE = process.env.TRANSCODER_VERBOSE || '0'
 // Operating mode:
 // local
@@ -11,71 +12,91 @@ const TRANSCODER_VERBOSE = process.env.TRANSCODER_VERBOSE || '0'
 const TRANSCODE_OPERATING_MODE = process.env.TRANSCODE_OPERATING_MODE || 'both'
 const TRANSCODE_EAE_LOCALLY = process.env.TRANSCODE_EAE_LOCALLY || false
 
-const { spawn } = require('child_process');
 var ON_DEATH = require('death')({debug: true})
+
+ON_DEATH( (signal, err) => {
+    console.log('ON_DEATH signal detected')
+    console.error(err)
+    process.exit(signal)
+})
+
+const { spawn } = require('child_process');
 
 var jobPoster = require('./jobPoster')
 
-if (TRANSCODE_OPERATING_MODE == 'local') {
-    transcodeLocally(process.cwd(), process.argv.slice(2), process.env)
-} else if (TRANSCODE_EAE_LOCALLY && process.argv.slice(2).filter(s => s.includes('eae_prefix')).length > 0) {
-    console.log('EasyAudioEncoder used, forcing local transcode')
-    transcodeLocally(process.cwd(), process.argv.slice(2), process.env)
-} else {
-    function setValueOf(arr, key, newValue) {
-        let i = arr.indexOf(key)
-        if (i > 0) {
-            arr[i+1] = newValue
-        }
-    }
-
-    let newArgs = process.argv.slice(2).map((v) => {
-        return v
-            .replace('127.0.0.1:', `${PMS_IP}:`)
-            .replace('aac_lc', 'aac');  // workaround for error -> Unknown decoder 'aac_lc'
-    })
-
-    if (TRANSCODER_VERBOSE == '1') {
-        console.log('Setting VERBOSE to ON')
-        setValueOf(newArgs, '-loglevel', 'verbose')
-        setValueOf(newArgs, '-loglevel_plex', 'verbose')    
-    }
-
-    let environmentVariables = process.env
-    let workDir = process.cwd()
-
-    console.log(`Sending request to orchestrator on: ${ORCHESTRATOR_URL}`)
-    if (TRANSCODER_VERBOSE == '1') {
-        console.log(`cwd => ${JSON.stringify(workDir)}`)
-        console.log(`args => ${JSON.stringify(newArgs)}`)
-        console.log(`env => ${JSON.stringify(environmentVariables)}`)
-    }
-    
-    jobPoster.postJob(ORCHESTRATOR_URL, 
-    {
-        type: 'transcode',
-        payload: {
-            cwd: workDir,
-            args: newArgs,
-            env: environmentVariables
-        }
-    },
-    (response) => {
-        if (!response.result) {
-            console.error('Distributed transcoder failed, calling local')
-            if (TRANSCODE_OPERATING_MODE == 'both') {
-                transcodeLocally(process.cwd(), process.argv.slice(2), process.env);
-            } else {
-                // remote only
-                console.error(`Error transcoding and local transcode is disabled: TRANSCODE_OPERATING_MODE=${TRANSCODE_OPERATING_MODE}`)
-                process.exit(1)
+// -- Relay proxy setup -- 
+var proxy = require('express-http-proxy');
+var app = require('express')();
+var server = require('http').createServer(app);
+app.use('/', proxy(`${PMS_IP}:${PMS_PORT}`));
+server.listen(0, () => {
+    console.log(`Relay Server listening on port ${server.address().port}`);
+    doTranscode(server.address().port);
+});
+// -----------------------
+  
+function doTranscode(relayPort) {
+    if (TRANSCODE_OPERATING_MODE == 'local') {
+        transcodeLocally(process.cwd(), process.argv.slice(2), process.env)
+    } else if (TRANSCODE_EAE_LOCALLY && process.argv.slice(2).filter(s => s.includes('eae_prefix')).length > 0) {
+        console.log('EasyAudioEncoder used, forcing local transcode')
+        transcodeLocally(process.cwd(), process.argv.slice(2), process.env)
+    } else {
+        function setValueOf(arr, key, newValue) {
+            let i = arr.indexOf(key)
+            if (i > 0) {
+                arr[i+1] = newValue
             }
-        } else {
-            console.log("Remote Transcoding was successful")
-            process.exit(0)
         }
-    }
-    )
+    
+        let newArgs = process.argv.slice(2).map((v) => {
+            return v
+                .replace(`127.0.0.1:${PMS_PORT}`, `${PMS_IP}:${relayPort}`) // set progress callback to relay port
+                .replace('aac_lc', 'aac');  // workaround for error -> Unknown decoder 'aac_lc'
+        })
+    
+        if (TRANSCODER_VERBOSE == '1') {
+            console.log('Setting VERBOSE to ON')
+            setValueOf(newArgs, '-loglevel', 'verbose')
+            setValueOf(newArgs, '-loglevel_plex', 'verbose')    
+        }
+    
+        let environmentVariables = process.env
+        let workDir = process.cwd()
+    
+        console.log(`Sending request to orchestrator on: ${ORCHESTRATOR_URL}`)
+        if (TRANSCODER_VERBOSE == '1') {
+            console.log(`cwd => ${JSON.stringify(workDir)}`)
+            console.log(`args => ${JSON.stringify(newArgs)}`)
+            console.log(`env => ${JSON.stringify(environmentVariables)}`)
+        }
+        
+        jobPoster.postJob(ORCHESTRATOR_URL, 
+        {
+            type: 'transcode',
+            payload: {
+                cwd: workDir,
+                args: newArgs,
+                env: environmentVariables
+            }
+        },
+        (response) => {
+            if (!response.result) {
+                console.error('Distributed transcoder failed, calling local')
+                if (TRANSCODE_OPERATING_MODE == 'both') {
+                    transcodeLocally(process.cwd(), process.argv.slice(2), process.env);
+                } else {
+                    // remote only
+                    console.error(`Error transcoding and local transcode is disabled: TRANSCODE_OPERATING_MODE=${TRANSCODE_OPERATING_MODE}`)
+                    process.exit(1)
+                }
+            } else {
+                console.log("Remote Transcoding was successful")
+                process.exit(0)
+            }
+        }
+        )
+    }    
 }
 
 function transcodeLocally(cwd, args, env) {
@@ -95,9 +116,3 @@ function transcodeLocally(cwd, args, env) {
         process.exit(withErrors);
     });
 }
-
-ON_DEATH( (signal, err) => {
-    console.log('ON_DEATH signal detected')
-    console.error(err)
-    process.exit(signal)
-})
